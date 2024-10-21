fc698318c86358b199e13fdd91bfa841d1fb4c2e
rust_lang_lover <rust_lang_lover@proton.me>
2024-10-21 13:09:40 +0300

    // moved to a separate function

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,9 @@
-mod connect_to_vps;
-mod command_executor;
-mod read_json_config;
+mod run_manager;
 
-use crate::connect_to_vps::{connect_to_vps};
-use crate::read_json_config::{read_json_config};
+use crate::run_manager::run_manager;
 
 fn main() -> () {
-    match read_json_config() {
-        Ok(data) => {
-            match connect_to_vps(data.user, data.password, data.host) {
-                Ok(()) => {
-                    println!("Successfully executed");
-                    return;
-                }
-                Err(err) => {
-                    println!("{}", err);
-                    return;
-                }
-            }
-        }
-        Err(err) => {
-            println!("{}", err)
-        }
+    match run_manager() {
+
     }
 }
---
+++ src/run_manager.rs
@@ -0,0 +1,22 @@
+use crate::connect_to_vps::connect_to_vps;
+use crate::read_json_config::{read_json_config, ReadJsonError};
+
+pub fn run_manager() -> Result<(), ReadJsonError> {
+    match read_json_config() {
+        Ok(data) => {
+            match connect_to_vps(data.user, data.password, data.host) {
+                Ok(()) => {
+                    println!("Successfully executed");
+                    return Ok(());
+                }
+                Err(err) => {
+                    println!("{}", err);
+                    return Ok(());
+                }
+            }
+        }
+        Err(err) => {
+            return Err(err)
+        }
+    }
+}
